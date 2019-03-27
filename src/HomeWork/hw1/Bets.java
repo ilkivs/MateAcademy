7aad40c8e1985bc7b1f40c7c6af02af37aa561a1
ist1997 <ilkivsviatoslav@gmail.com>
2019-03-27 13:34:55 +0200

    Homework 1 (final version)

---
+++ src/HomeWork/hw1/Bets.java
@@ -0,0 +1,14 @@
+package HomeWork.hw1;
+
+/*
+    Task 1.1
+ */
+
+public class Bets {
+
+    public int footballMatch(int real1, int real2, int bet1, int bet2) {
+        int nothingGuessed = ((real1 > real2 && bet1 <= bet2) || (real1 < real2 && bet1 >= bet2) ||
+                (real1 >= real2 && bet1 < bet2) || (real1 <= real2 && bet1 > bet2)) ? 0 : 1;
+        return (real1 == bet1 && real2 == bet2) ? 2 : nothingGuessed;
+    }
+}
---
+++ src/HomeWork/hw1/Car.java
@@ -0,0 +1,48 @@
+package HomeWork.hw1;
+
+/*
+    Task 1.7
+ */
+
+public class Car implements Cloneable {
+    private String manufacturer;
+    private String model;
+    private int releaseYear;
+
+    public Car(String manufacturer, String model, int releaseYear) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Car(getManufacturer(), getModel(), getReleaseYear());
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Manufacturer: ").append(getManufacturer())
+                .append(", Model: ").append(getModel())
+                .append(", Release Year: ").append(getReleaseYear())
+                .append(", hashCode: ").append(hashCode());
+        return sb.toString();
+    }
+}
