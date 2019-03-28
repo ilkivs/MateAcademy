0c186a2dc2def964c3d0865d84efc31e833f971a
ist1997 <ilkivsviatoslav@gmail.com>
2019-03-28 13:36:24 +0200

    Homework 3 (done)

---
+++ src/HomeWork/hw3/Main.java
@@ -0,0 +1,22 @@
+package HomeWork.hw3;
+
+import HomeWork.hw3.task1.MergeSort;
+import HomeWork.hw3.task2.StagePlay;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("===== Task 1 =====");
+        MergeSort mergeSort = new MergeSort();
+        System.out.println("Two arrays before merge sort:");
+        mergeSort.printStartArrays();
+        mergeSort.mergeArrays();
+        System.out.println("Result array after merge sort:");
+        mergeSort.printResultArray();
+
+        System.out.println("\n===== Task 2 =====");
+        StagePlay stagePlay = new StagePlay();
+        stagePlay.groupTextByRoles();
+        stagePlay.printGroupedText();
+    }
+}
